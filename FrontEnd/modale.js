526e14db4d9bfcb457a969197c6b7066c1f8880a
BttCrtd <courtadon.baptiste@gmail.com>
2024-10-22 10:07:33 +0200

    Gestion de la fermeture de la modale

--- FrontEnd/modale.js
+++ FrontEnd/modale.js
@@ -3,12 +3,28 @@ function afficherModale(){
     modalePopup.classList.add("active")
 }
 
+function cachermodale(){
+    let modalePopup = document.querySelector('.modale')
+    modalePopup.classList.remove("active")
+}
+
 function initAddEventListenerModale(){
     const buttonEdit = document.querySelector(".edit-btn")
-    //let modalePopup = document.querySelector(".modale")
+    const modalePopup = document.querySelector(".modale")
+    const closeButton = document.querySelector(".fa-x")
     buttonEdit.addEventListener("click", () => {
         afficherModale()
     })
+    modalePopup.addEventListener("click", (event) => {
+        if(event.target === modalePopup || event.targer === closeButton){
+            cachermodale()
+        }
+    })
+    closeButton.addEventListener("click", (event) => {
+        if(event.target === closeButton){
+            cachermodale()
+        }
+    }) 
 }
 
 initAddEventListenerModale()
